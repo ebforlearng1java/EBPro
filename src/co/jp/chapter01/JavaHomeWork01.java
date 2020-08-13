a35eced9dd9c562be0d1407ae134aa59f9a8d2c8
14072 <14072@DESKTOP-NEP2GT3>
2020-08-13 13:03:17 +0900

    鄧維 2020/08/13 質問回答

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,32 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		/** 01型＆式 */
+
+		//質問１︓以下変数nの値は︖
+		int i = 10;
+		int n = i % 5;
+		System.out.println("n=" + 0);
+
+		//質問２︓ 右の式を解釈してください。a = a > 10 ? 11 : 0;
+		System.out.println("aは10以下の場合、aの値が0とする");
+		System.out.println("a=" + 0);
+		System.out.println("aは10より大きい場合、aの値が11とする");
+		System.out.println("a=" + 11);
+
+		//質問３︓変数値を比較する演算子は = or == ?
+		System.out.println("==");
+
+		//質問４︓ブリアン（boolean）変数に設定可能の値は「?」「?」。
+		System.out.println("true & false");
+
+		//質問５︓下記コードで、変数xとyの値を回答してください。
+		int x = 10;
+		int y = 20;
+		x = y;
+		y = 30;
+		System.out.println("x=" + 20);
+		System.out.println("y=" + 30);
 	}
 
 }
