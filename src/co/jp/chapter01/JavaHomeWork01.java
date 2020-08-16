1e42db5e35bfefbb0713b60328b3031ca18801e2
LIN <LIN@iceland>
2020-08-16 11:07:26 +0900

    20200816 chapter01 質問回答

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,24 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//1質問１︓以下変数nの値は︖
+		int i = 10;
+		int n = i % 5;
+		System.out.println(n);
+		//実行結果：0
+
+		//質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+		//	
+		//		答え：?記号前の式（a > 10）を評価する、trueの場合、11を返す、falseの場合、0を返す。
+		//
+		//質問３︓変数値を比較する演算子は = or == ?
+		//
+		//		答え：変数値を比較する演算子は == です。
+		//	
+		//質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		//	
+		//		答え：ブリアン（ boolean ）変数に設定可能の値は「true」「false」。
+
 	}
 
 }
