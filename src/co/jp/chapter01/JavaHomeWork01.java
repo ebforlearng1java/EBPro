d05cf1d2d44ae73574f35a630e6a3b83d8bcb0e6
amano <amano@192.168.1.3>
2020-08-18 00:56:25 +0900

    シャン 08180056

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -4,6 +4,20 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問1:以下変数nの値は?
+		int i = 10;
+		int n = i%5;
+		System.out.println(n);
+		//0
+
+		//質問2:右の式を解釈してください: a=a>10?11:0;
+		// aは10より大きなら　a=11を返す、そでなければ　a=0を返す
+
+		//質問3:変数値を比較する演算子は
+		// ==
+
+		//質問4:ブリアン( boolean )変数に設定可能の値は「?」「?」。
+		//true また false
 
 	}
 
