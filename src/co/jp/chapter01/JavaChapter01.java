6a18f422a8af65030c5425cf02f9f3e7d009142c
amano <amano@192.168.1.3>
2020-08-18 00:47:55 +0900

    シャンシャン

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -5,11 +5,32 @@ public class JavaChapter01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		System.out.println("Hello World!");
+
+		//変数の宣言
 		boolean flg = true;
 		boolean flg1 = true;
 
 		System.out.println(flg);
 		System.out.println(flg1);
+
+		byte pony =1;
+		System.out.println(pony);
+
+		int pony32 = 1;
+		System.out.println(pony32);
+
+		float flt =1.1f;
+		System.out.println(flt);
+
+		Integer pony32_1= 123;
+		System.out.println(pony32_1);
+
+		byte pony8 = 0;
+		System.out.println("byte:" + pony8);
+
+		Byte pony8_1 = null;
+		System.out.println("Byte:" + pony8_1);
 	}
 
 }
