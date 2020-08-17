aeafe9d114ee11a5a4e03f72d6175f827584b9a6
Everbe <a410526712@yahoo.co.jp>
2020-08-17 20:20:55 +0900

    Commit test

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -3,7 +3,7 @@ package co.jp.chapter01;
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
-		// TODO 鑷嫊鐢熸垚銇曘倢銇熴儭銈姐儍銉夈兓銈广偪銉�
+		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("commitng testing");
 	}
 
