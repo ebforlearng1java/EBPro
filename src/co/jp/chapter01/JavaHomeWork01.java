fcdbfbb2df226dca60e0ef505519b757fa4eae96
user <user@192.168.0.104>
2020-08-18 23:58:07 +0900

    2020/08/18/ QJN

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -3,8 +3,12 @@ package co.jp.chapter01;
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("質問1");
+    	int i = 10;
+		int n = i % 5;
+		System.out.println("nの値は" + n);
 
 	}
-
 }
+
