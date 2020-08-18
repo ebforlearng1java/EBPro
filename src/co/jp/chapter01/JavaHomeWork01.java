cd02058b647ac83a9fe21531755d6fa7bc7523d9
XiaoMeng <XiaoMeng@192.168.0.12>
2020-08-18 17:43:59 +0900

    肖

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -4,7 +4,31 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println（"HELLO WORLD!"）；
 	}
-
 }
+
+/*
+ 解答(P3)：
+6. 下記コードで、変数 x と y の値を回答してください。
+解答：
+x = 20
+y = 30 
+
+解答(P6)：
+質問１：
+n = 0
+
+質問２：
+a > 10を評価します。
+Trueの場合、11を返します。
+Falseの場合、０を返します。
+
+質問３：
+「=」は値の代入を意味する演算子です。右の値を左に入れます。
+「==」は値の比較を意味する演算子です。右の値と左の値を比較します。
+
+質問４：
+True/False
+ */
+ 
