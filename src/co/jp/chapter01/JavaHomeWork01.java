65a8d0e058ff0bfddc2af82ec6f443353558af39
andyh <andyh@DESKTOP-V0EV2LH>
2020-08-18 21:40:24 +0900

    2020/8/18 胡 宿題提出

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -4,7 +4,21 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+	//質問1
+    int i = 10;
+    int n = i%5;
+    System.out.println("int n = "+ n);
 
+     //質問2
+    //a = a > 10 ? 11 : 0を解釈
+    System.out.println("aが10より大きい場合は11をaに代入して、逆な場合は0をaに代入する。");
+
+    //質問3
+    System.out.println("変数値を比較する演算子は==です。");
+
+    //質問4
+    System.out.println("変数ブリアンに設定可能な値はTrueとFalse");
 	}
+
 
 }
