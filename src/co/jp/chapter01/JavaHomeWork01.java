661e8128ba83efb250f133ccb7aec9445c1946e5
13254 <13254@DESKTOP-08SP42E>
2020-08-19 00:20:07 +0900

    KNZW

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,11 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		System.out.println("question1");
+		System.out.println("questiong1");
+		int i = 10;
+		int n = i % 5;
+		System.out.println("nの値"+n);
 	}
 
 }
