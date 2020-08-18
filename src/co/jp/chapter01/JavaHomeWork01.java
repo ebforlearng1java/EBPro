1491a467a96279e3566b5b5a86de961e11a41f36
user <user@192.168.0.104>
2020-08-19 00:52:58 +0900

    2020/8/19 QJN

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -4,10 +4,13 @@ public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("質問1");
-    	int i = 10;
-		int n = i % 5;
-		System.out.println("nの値は" + n);
+		System.out.println("質問");
+    	int x = 10;
+		int y = 20;
+		x = y;
+	    y = 30;
+		System.out.println(x);
+		System.out.println(y);
 
 	}
 }
