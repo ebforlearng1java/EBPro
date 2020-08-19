30135b921db765d846190c62f6ec5a75d810db1f
User <DengWei>
2020-08-19 21:48:51 +0900

    鄧維 2020/08/19 宿題提出

--- src/co/jp/chapter00/JavaHomeWork00.java
+++ src/co/jp/chapter00/JavaHomeWork00.java
@@ -5,12 +5,16 @@ public class JavaHomeWork00 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		/** 00 基本の基本 */
+
 		// 質問１　今現場に自分担当のフェーズと成果物
-		// 答え～～～
+		// 答え:製造とUTテスト（CDPT）
+		//      成果物：ソースコード、テストケース一覧、テストコード、テストデータ、テスト結果
+
 		// 質問２　今現場に使用しているIDE　（開発ツール）
 		// 開発言語向け
 		// DB向け
-
+		//答え：eclipse、java、DB2
 	}
 
 }
