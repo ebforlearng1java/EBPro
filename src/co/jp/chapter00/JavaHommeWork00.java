01be87843998c99477eb7310e9c0e48be91fd7d0
amano <SQM>
2020-08-19 20:19:36 +0900

    シャン　HW00

---
+++ src/co/jp/chapter00/JavaHommeWork00.java
@@ -0,0 +1,16 @@
+package co.jp.chapter00;
+
+public class JavaHommeWork00 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		// 質問１　今現場に自分担当のフェーズと成果物
+		// 答え～～～
+		// 質問２　今現場に使用しているIDE　（開発ツール）
+		// 開発言語向け
+		// DB向け
+
+	}
+
+}
