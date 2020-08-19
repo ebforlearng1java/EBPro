60683121ee0f23b26e8130ec013e911d7f150bdb
Everbe <a410526712@yahoo.co.jp>
2020-08-19 20:12:39 +0900

    test

---
+++ src/co/jp/chapter00/JavaHomeWork01.java
@@ -0,0 +1,15 @@
+package co.jp.chapter00;
+
+public class JavaHomeWork01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//質問１　今現場に自分担当のフェーズと成果物
+		//答え；
+		
+		//質問２　今現場に使用しているIDE　（開発ツール）
+		//開発言語；
+		//DB；
+	}
+
+}
