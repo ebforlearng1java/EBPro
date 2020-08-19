29c654c9b371ba65efefc30c63890cc070e85e97
User <DengWei>
2020-08-19 21:52:12 +0900

    鄧維 2020/08/19 練習

--- src/co/jp/chapter02/JavaChapter02.java
+++ src/co/jp/chapter02/JavaChapter02.java
@@ -4,7 +4,13 @@ public class JavaChapter02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		String name = "DW";
+		System.out.println(name);
 
+		String temp1 = "equals";
+		String temp2 = new String("equals");
+
+		System.out.println(temp1==temp2);
 	}
 
 }
--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -5,6 +5,31 @@ public class JavaHomeWork02 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		/** 02 型（文字列） */
+
+		//質問１︓ System.out.println(“this is line1\tthis line2“); は何行出力しますか？
+		//1. １行　2. ２行
+		//答え: 1. １行
+
+		//質問２︓Stringを比較方法は？
+		//1. == 2. equals
+		//答え：2. equals
+
+		//質問３︓Float値⇒String文字列変更する場合、どうなメソッドを利用しますか
+		//1. String.valueOf("1.0F") 2. 1.0F + ""
+		//答え：1と2が両方利用できる
+
+		//質問４︓以下式の値を記載してください。
+		//1. 1.0 + 2.0
+		//2. 1.0 + “2.0“
+		//答え：1.  数値 3.0  2.文字列 "1.02.0"
+
+		//質問５︓以下ステートメントの出力を記載してください。
+		//public static void main(String[] args) {
+		//int a = 10;
+		//System.out.println("a");
+		//}
+		//答え： a
 	}
 
 }
