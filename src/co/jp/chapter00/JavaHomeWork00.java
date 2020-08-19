d1890086c8423e5c9ec29e0b1dde01de7e86c27d
LIN <LIN@iceland>
2020-08-19 22:25:22 +0900

    20200819 巩 chapter00 質問回答

---
+++ src/co/jp/chapter00/JavaHomeWork00.java
@@ -0,0 +1,21 @@
+package co.jp.chapter00;
+
+public class JavaHomeWork00 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		// 質問１　今現場に自分担当のフェーズと成果物
+		// 回答：単体テストUT　　成果物：UCL（改修）、UTエビデンス
+		//		 結合テストIT　　成果物：ICL（理解）、ITaエビデンス
+
+		// 質問２　今現場に使用しているIDE　（開発ツール）
+		// 			開発言語向け
+		// 			DB向け
+		// 回答：デバッグツール：Visio studio
+		// 		 言語：VB、Jquery、Asp
+		// 		 DB：SQL Server
+
+	}
+
+}
