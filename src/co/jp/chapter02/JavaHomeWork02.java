24f003f6ed7a6a11f58b435927eadf48ca20bcb8
andyh <andyh@DESKTOP-V0EV2LH>
2020-08-24 21:19:09 +0900

    2020/8/24 胡

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,6 +4,24 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問1
+		System.out.println("1行");
+
+		//質問2
+		System.out.println("equals");
+
+		//質問3
+		System.out.println("String.valueOf(1.0F)");
+
+		//質問4
+		System.out.println("3.0");
+		System.out.println("1.02.0");
+
+		//質問5
+		//出力されるべきなのは文字列のa
+		System.out.println("a");
+
+
 
 	}
 
