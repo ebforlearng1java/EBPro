bd2971d63af23e5ebc3b92281224fbdde3e545eb
xu <xu@192.168.0.13>
2020-08-26 20:30:19 +0900

    true,false

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -5,16 +5,22 @@ public class JavaChapter03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	
-		
-	if (true) 
-	{
-		 //trueの処理ロジック
-	} 
-	else 
-	{
-		//falseの処理ロジック
-	}
+
+		int i = 11;
+		if (i > 0)
+		{
+			//ア　上記条件はTRUEの場合、下記処理をを実行
+			System.out.println("変数,iはプラス");
+			 //trueの処理ロジック
+		} else if (i < 0) {
+			//イ　ア条件がFAlSEの場合、下記の処理を実行
+			System.out.println("変数,iはマイナス");
+		}
+		else
+		{
+			System.out.println("変数,iはゼロ");
+			//falseの処理ロジック
+		}
 
 	}
 
