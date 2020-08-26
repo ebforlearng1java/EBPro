1d37812c6d88ee92651bcf92816a9c17e05fd7a6
ユーザー <>
2020-08-26 22:01:25 +0900

    授業練習_20200826_呉龍

--- src/co/jp/chapter02/JavaChapter02.java
+++ src/co/jp/chapter02/JavaChapter02.java
@@ -4,6 +4,55 @@ public class JavaChapter02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		String name_01 = "WL";
+		String name_02 = "WL";
+
+		System.out.println(name_01);
+		System.out.println(name_02);
+		int num = 10;
+		if (num>0 && num<9) {
+			System.out.println("変数numはプラスです！");
+		}
+		else if(num > -9 && num < 0) {
+			System.out.println("変数numはマイナスです！");
+		}
+		else if (num == 0) {
+			System.out.println("変数numはゼロです！");
+		}
+		else {
+			System.out.println("変数numは[-9,9]の範囲外です！");
+		}
+
+	    num = 96;
+		switch(num) {
+			case 9 :
+			case 8 :
+			case 7 :
+			case 6 :
+			case 5 :
+			case 4 :
+			case 3 :
+			case 2 :
+			case 1 :
+				System.out.println("変数numはプラスです！");
+				break;
+			case -9 :
+			case -8 :
+			case -7 :
+			case -6 :
+			case -5 :
+			case -4 :
+			case -3 :
+			case -2 :
+			case -1 :
+				System.out.println("変数numはマイナスです！");
+				break;
+			case 0 :
+				System.out.println("変数numはゼロです！");
+				break;
+			default:
+				System.out.println("変数numは[-9,9]の範囲外です！");
+		}
 
 	}
 
--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -4,6 +4,61 @@ public class JavaChapter03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		int mark = 101;
+
+		// 成績は[90,100]の間の場合、成績ランキングはAです。
+		if (mark >=90 && mark <=100) {
+			System.out.println("成績ランキング判断： A");
+		}
+
+		// 成績は[80,90)の間の場合、成績ランキングはBです。
+		else if (mark<90 && mark >= 80) {
+			System.out.println("成績ランキング判断： B");
+		}
+
+		// 成績は[70,80)の間の場合、成績ランキングはCです。
+		else if (mark<80 && mark >= 70) {
+			System.out.println("成績ランキング判断： C");
+		}
+
+		// 成績は[60,70)の間の場合、成績ランキングはDです。
+		else if (mark<70 && mark >= 60) {
+			System.out.println("成績ランキング判断： D");
+		}
+
+		// 成績は[0,60)の間の場合、成績ランキングはEです。
+		else if (mark<60 && mark >= 0){
+			System.out.println("成績ランキング判断： E");
+		}
+
+		// 入力数字が[0,100]以外の場合、メッセージ「入力ミス」を出力する。
+		else {
+			System.out.println("入力ミス");
+		}
+
+		// forループ
+		for(int num = 0;num <=10; num++) {
+			System.out.println(num);
+		}
+
+		// forループ
+		for(int num = 0;num <10; num++) {
+			System.out.println(num);
+		}
+
+		// whileループ
+		int num = 0;
+		while(num <=10) {
+			System.out.println(num);
+			num++;
+		}
+
+		// do...whileループ
+		num = 0;
+		do {
+			System.out.println(num);
+			num++;
+		}while(num <10);
 
 	}
 
