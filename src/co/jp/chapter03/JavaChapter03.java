576b1261673397f4debe9271b908469461d0e3f3
User <DengWei>
2020-08-26 22:09:52 +0900

    鄧維 2020/8/26 宿題提出

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -5,6 +5,90 @@ public class JavaChapter03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	}
+		//if-else if 条件判断
+		int i = 0;
+		if (i > 0 && i <= 9) {
+			//ア：上記条件がtrueの場合、下記処理を実行
+			System.out.println("変素.iはプラス");
+		} else if (i < 0 && i >= -9) {
+			//イ：ア条件がfalse　かつ、上記条件がtrueの場合、
+			//下記処理を実行
+			System.out.println("変素.iはマイナス");
+		} else if (i == 0) {
+			//ウ：ア条件がfalse　かつ、イ条件がfalseの場合、
+			//上記条件がtrueの場合、下記処理を実行
+			System.out.println("変素.iはゼロ");
+		} else {
+			//エ：ア条件がfalse　かつ、イ条件がfalseの場合、
+			//ウ条件がfalseの場合、下記処理を実行
+			//上記以外
+			System.out.println("input変数の値が計算対象外「-9～9」の間に指定してください");
+		}
 
+		//switch-case 条件判断
+		switch (i) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			System.out.println("変素.iはプラス");
+			break;
+		case -1:
+		case -2:
+		case -3:
+		case -4:
+		case -5:
+		case -6:
+		case -7:
+		case -8:
+		case -9:
+			System.out.println("変素.iはマイナス");
+			break;
+		case 0:
+			System.out.println("変素.iはゼロ");
+			break;
+		default:
+			System.out.println("input変数の値が計算対象外「-9～9」の間に指定してください");
+		}
+
+		//forループ
+		for (int a = 0; a < 10; a++) {
+			System.out.println(a);
+		}
+
+		for (int a = 0; a <= 10; a++) {
+			System.out.println(a);
+		}
+
+		//whileループ
+		int b=0;
+		while(b<10) {
+			System.out.println(b);
+			b++;
+		}
+
+		b=0;
+		while(b<=10) {
+			System.out.println(b);
+			b++;
+		}
+
+		//do-whileループ
+		int c=0;
+		do {
+			System.out.println(c);
+			c++;
+		}while(c<10);
+
+		c=0;
+		do {
+			System.out.println(c);
+			c++;
+		}while(c<=10);
+	}
 }
--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -5,6 +5,59 @@ public class JavaHomeWork03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		/**03 フロー制御 */
+
+		//質問︓以下コードを補完してください。
+		//boolean value = true;
+		//switch(value) {
+		//case ??:break; // case ??
+		//case ??:break; // case ??
+		//default:break; // default は必要ですか？
+		//}
+		//答え：switchにbooleanが使えない
+
+		//質問１︓成績ランキング判断を作成する
+		//「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓D, 60以下︓E」
+		//答え：
+		int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+
+		if (a >= 90 && a<=100) {
+			//ア：上記条件がtrueの場合、下記処理を実行
+			System.out.println("A");
+		} else if (a < 90 && a >= 80) {
+			//イ：ア条件がfalse　かつ、上記条件がtrueの場合、
+			//下記処理を実行
+			System.out.println("B");
+		} else if (a < 80 && a >= 70) {
+			//ウ：ア、イ条件がfalseの場合、かつ、
+			//上記条件がtrueの場合、下記処理を実行
+			System.out.println("C");
+		} else if (a < 70 && a >= 60) {
+			//エ：ア、イ、ウ条件がfalseの場合、かつ、
+			//上記条件がtrueの場合、下記処理を実行
+			System.out.println("D");
+		} else if (a < 60 && a >= 0){
+			//オ：ア、イ、ウ、エ条件がfalseの場合、かつ、
+			//上記条件がtrueの場合、下記処理を実行
+			System.out.println("E");
+		}else {
+			//カ：ア、イ、ウ、エ、オ条件がfalseの場合、
+			//上記以外、下記処理を実行
+			System.out.println("aの値が正しくない");
+		}
+
+		//質問２︓日本のコインを１円、５円、１０円、１００円、５００円があります。
+		//３３４０円のコイン数をもとめください。
+		//答え：13個。500円のコイン数は6個、100円のコイン数は3個、10円のコイン数は4個
+		int count500=3340/500;
+		int count100=3340%500/100;
+		int count10=3340%500%100/10;
+		int total=count500+count100+count10;
+		System.out.println("全てコイン数は"+total);
+		System.out.println("500円のコイン数は"+count500);
+		System.out.println("100円のコイン数は"+count100);
+		System.out.println("10円のコイン数は"+count10);
 	}
 
 }
