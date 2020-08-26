6138f732becd5baa1fc9ecc8d692eef8b2517a20
xu <xu@192.168.0.13>
2020-08-26 22:08:36 +0900

    8/26

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -5,23 +5,60 @@ public class JavaChapter03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-
-		int i = 11;
-		if (i > 0)
+		
+		int i = 0;
+		if (i > 0) 
 		{
 			//ア　上記条件はTRUEの場合、下記処理をを実行
 			System.out.println("変数,iはプラス");
-			 //trueの処理ロジック
 		} else if (i < 0) {
-			//イ　ア条件がFAlSEの場合、下記の処理を実行
+			//イ　ア条件がFAlSE、かつ上記の条件がTRUEの場合、下記の処理を実行
 			System.out.println("変数,iはマイナス");
 		}
-		else
+		else 
 		{
+			//アの条件がFLASE、かつイの条件もFLASEの場合、下記の処理を実行
 			System.out.println("変数,iはゼロ");
-			//falseの処理ロジック
 		}
 
+		//switch
+		switch (i) {
+		case -1:
+		case -2:
+		case -3:
+	    case -4:
+	    case -5:
+	    case -6:	
+	    case -7:
+	    case -8:
+	    case -9:	
+	    System.out.println("変数,iはマイナス");	
+	    break;
+	    case 1:
+		case 2:
+		case 3:
+	    case 4:
+	    case 5:
+	    case 6:	
+	    case 7:
+	    case 8:
+	    case 9:	
+	    System.out.println("変数,iはプラス");		
+	    break;
+	    case 0:
+	    System.out.println("変数,iは0");
+	    break;
+	    default:
+	    System.out.println("計算対象外");
+		}
+		
+		
+		//loopの一つはFORループ
+		//自然数 1 2 3 4 5
+		//0はJAVAの一番目の位置
+		//
+		for (int a = 0; a < 10; a++) {
+		System.out.println(a);}
 	}
 
 }
