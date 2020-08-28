5e183f9246c23931e53a6ba3f92fdc221ad2247c
qimin <qimin@DESKTOP-EF06LPN>
2020-08-28 18:34:48 +0900

    シャン　0828 1834

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -84,68 +84,134 @@ public class JavaChapter03 {
 		//自然数　1,2,3,4,5,
 		//0 はJAVAの一番目の位置
 		//　index
-
-		int con1=0;
-		for(int i = 0; i < 100 ;i++) {
-			//System.out.println(i);
-			con1 = con1 +1;
-		}
-		System.out.println(con1);
-
-		int con2=0;
-		for(int i = 0; i <=100 ;i++) {
-			//System.out.println(i);
-			con2 = con2 +1;
-		}
-		System.out.println(con2);
+//
+//		int con1=0;
+//		for(int i = 0; i < 100 ;i++) {
+//			//System.out.println(i);
+//			con1 = con1 +1;
+//		}
+//		System.out.println(+con1);
+//
+//		int con2=0;
+//		for(int i = 0; i <=100 ;i++) {
+//			//System.out.println(i);
+//			con2 = con2 +1;
+//		}
+//		System.out.println(con2);
+//
+//		//while
+//
 
 
 		//while の使い方
-		int a = 0 ;
+//		int a = 0 ;
 //		while(a<10) {
 //			System.out.println(a);
 //			a++;
 //		}
 
-		while(true) {
-			System.out.println(a);
-			if (a>=10) {
-				break;
-			}
-			a++;
+		//deal loop 下方のソースは→dead code
+		//if (true) のelse にも→　dead code デットロジック
+		//break 方の下も→　dead code デットロジック
 
+//		int num1=0;
+//		while(num1<10) {
+//			System.out.println("なし"+num1);
+//			num1++;
+//		}
+//
+//		System.out.println("-------------");
+//
+//		int num2=0;
+//		while(num2<=10) {
+//			System.out.println("あり"+num2);
+//			num2++;
+//		}
+//
+//		System.out.println("*********************");
+//
+//		//do while
+//		//現場ではデータソースの状態を調べる、例えば10秒ごと
+//		int x=0;
+//		do {
+//			System.out.println("do while no LABALABA"+x);
+//			x++;
+//		}while(x<10);
+//
+//		System.out.println("+++++++++++++++++++++");
+
+//
+//		while(true) {
+//			System.out.println(a);
+//			if (a>=10) {
+//				break;
+//			}
+//			a++;
+//
+//		}
+//
+//		//do while は必ず1回実行する
+//		int x = 100;
+//		do {
+//			System.out.println(x);
+//			x++;
+//		}while(x<10);
+//
+//
+//		//brake文の使用
+//		for(int i=0; i<100;i++) {
+//			if(i>10) {
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+//
+//		//continueの使い方
+//		for (int i=0;i<100;i++) {
+//			if(i%2==0) {
+//				System.out.println(i);
+//			}
+//		}
+//
+//		//スキップの意味
+//		for(int i=0; i<100; i++) {
+//			if(i%2 !=0 ) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+
+//		int M=7;
+//		for(int i=0;i<M*M;i++) {
+//			System.out.print("*");
+//			if(i>0 && (i+1)%M==0) {
+//				System.out.println();
+//			}
+//		}
+//
+//
+//		int N=7;
+//		for(int i=0;i<N;i++ ) {
+//			for(int j=0;j<N;j++) {
+//				System.out.print("+");
+//			}
+//			System.out.println("");
+//		}
+
+		int N=10;
+		for(int i=0;i<N;i++ ) {
+			for(int j=0;j<N;j++) {
+				if(i==j || i+j==N-1) {
+					System.out.print(i+""+j+" ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println("");
 		}
 
-		//do while は必ず1回実行する
-		int x = 100;
-		do {
-			System.out.println(x);
-			x++;
-		}while(x<10);
 
-
-		//brake文の使用
-		for(int i=0; i<100;i++) {
-			if(i>10) {
-				break;
-			}
-			System.out.println(i);
-		}
-
-		//continueの使い方
-		for (int i=0;i<100;i++) {
-			if(i%2==0) {
-				System.out.println(i);
-			}
-		}
-
-		//スキップの意味
-		for(int i=0; i<100; i++) {
-			if(i%2 !=0 ) {
-				continue;
-			}
-			System.out.println(i);
-		}
 
 
 
--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -5,6 +5,35 @@ public class JavaChapter04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//配列　数　組
+
+		//整形配列
+		int [] array = new int[] {1,2,3,4,5};
+
+		//文字列配列
+		String [] strArray = new String[] {"1","2","3","4","5"};
+
+		//
+		Object[] objArray = new Object[] {1,"333",true};
+
+
+		//定義方式　宣言　かつ　初期化 値
+		int [] intArray1 = new int[] {55,22,33,11,22,555};
+		for(int i=0; i< intArray1.length ; i++) {
+			System.out.println(intArray1[i]);
+		}
+
+
+		//定義方式　宣言　かつ 長さ定義のみ
+		int [] intArray2 = new int[4] ;
+		for(int i=0; i< intArray2.length ; i++) {
+			System.out.println(intArray2[i]);
+		}
+
+
+
+
+
 	}
 
 }
