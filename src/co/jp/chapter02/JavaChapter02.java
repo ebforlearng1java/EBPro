1e40e6b5fb1c33b3846291018a67bc462b00407c
User <DengWei>
2020-08-29 23:34:02 +0900

    鄧維 2020/08/29 プッシュ

--- src/co/jp/chapter02/JavaChapter02.java
+++ src/co/jp/chapter02/JavaChapter02.java
@@ -4,13 +4,22 @@ public class JavaChapter02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+
 		String name = "DW";
 		System.out.println(name);
 
+		// 変数宣言の方式１
 		String temp1 = "equals";
 		String temp2 = new String("equals");
 
-		System.out.println(temp1==temp2);
+		System.out.println("変数の宣言（方式１）:"+ (temp1==temp2));
+
+		// 変数宣言の方式２
+		String temp3 = new String("equals");
+		String temp4 = new String("equals");
+
+		System.out.println("変数の宣言（方式２）:"+ (temp3 == temp4)); //NG
+		System.out.println("変数の宣言（方式２）:"+ (temp3.equals(temp4)));//ok
 	}
 
 }
