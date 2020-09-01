d5124ba0da30983c15ea08098468e4087fdd6baa
andyh <andyh@DESKTOP-V0EV2LH>
2020-09-01 23:21:03 +0900

    2020/9/01 予習

--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -4,6 +4,25 @@ public class JavaChapter04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		int[][] arr2d = new int[][] {
+			{1,2,3,4},{21,22,23,24}
+		};
+		System.out.println(arr2d.length);
+
+		//int[] arr = new int[3];
+		//System.out.println(arr);
+
+		//配列の要素を出力
+		//方法1
+		int[] arr3 = new int[] {1,2,3,4,5};
+		for(int x:arr3) {
+			System.out.println(x);
+		}
+        //方法2
+		for(int i = 0;i<arr3.length;i++) {
+			int y = arr3[i];
+			System.out.println(y);
+		}
 
 	}
 
