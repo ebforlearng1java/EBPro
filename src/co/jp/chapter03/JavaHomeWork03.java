c4a0366018987356fcd960160ca3a4424e83cd7c
XiaoMeng <SM>
2020-09-01 12:22:00 +0900

    03フロー制御　宿題(再コミット)

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -21,18 +21,18 @@ public class JavaHomeWork03 {
 
 		//5ページ 質問１
 
-		int a = 56;
-		System.out.println("成績ランキング判断。入力値=" + a);
+		int q = 56;
+		System.out.println("成績ランキング判断。入力値=" + q);
 
-		if(a >= 90 && a<= 100) {
+		if(q >= 90 && q<= 100) {
 			System.out.println("A");
-		}else if(a >= 80 && a < 90) {
+		}else if(q >= 80 && q < 90) {
 			System.out.println("B");
-		}else if(a >= 70 && a < 80) {
+		}else if(q >= 70 && q < 80) {
 			System.out.println("C");
-		}else if(a >= 60 && a <70) {
+		}else if(q >= 60 && q <70) {
 			System.out.println("D");
-		}else if(a>=0 && a< 60) {
+		}else if(q>=0 && q< 60) {
 			System.out.println("E");
 		}else{
 			System.out.println("1から100までの数値をご入力ください。");
@@ -43,19 +43,19 @@ public class JavaHomeWork03 {
 		//5ページ 質問2
 
 		int m = 3340;
-		int round500 = (int)Math.floor(m/500);//floorは小数点以下を切り捨てる
-		int round100 = (int)Math.floor(m % 500 / 100);
-		int round50 = (int)Math.floor(m % 500 % 100 /50);
-		int round10 = (int)Math.floor(m % 500 % 100 % 50 /10);
-		int round5 = (int)Math.floor(m % 500 % 100 % 50 % 10 /5);
-		int round1 = (int)Math.floor(m % 500 % 100 % 50 % 10 % 5 /1);
+		int a = m/500;
+		int b = m % 500 / 10;
+		int c = m % 500 % 100 /50;
+		int d = m % 500 % 100 % 50 /10;
+		int e = m % 500 % 100 % 50 % 10 /5;
+		int f = m % 500 % 100 % 50 % 10 % 5 /1;
 		System.out.println(m + "円のコイン数は以下の通りです：");
-		System.out.println("500円のコインは" +round500 + "枚、");
-		System.out.println("100円のコインは" +round100 + "枚、");
-		System.out.println("50円のコインは" +round50 + "枚、");
-		System.out.println("10円のコインは" +round10 + "枚、");
-		System.out.println("5円のコインは" +round5 + "枚、");
-		System.out.println("1円のコインは" +round1 + "枚。");
+		System.out.println("500円のコインは" +a + "枚、");
+		System.out.println("100円のコインは" +b + "枚、");
+		System.out.println("50円のコインは" +c + "枚、");
+		System.out.println("10円のコインは" +d + "枚、");
+		System.out.println("5円のコインは" +e + "枚、");
+		System.out.println("1円のコインは" +f + "枚。");
 
 	}
 
