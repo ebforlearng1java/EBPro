b4bcb2fef81ffb0a6dc6aa08ed16b367a6415cba
User <DengWei>
2020-09-02 23:41:55 +0900

    鄧維 2020/09/01 宿題提出

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -5,6 +5,100 @@ public class JavaHomeWork04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	}
+		/**04 配列 */
+		//問題１︓int[] array = new int[4]; 各要素の値を記載してください。
+		//答え：
+		int[] array1 = new int[4];
+		System.out.println("array1の各要素の値は 0 です。");
+		array1[0] = 3;
+		array1[1] = 23;
+		array1[2] = 17;
+		array1[3] = 6;
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println(array1[i]);
+		}
 
+		//問題２︓int[] array = new int[] {5, 6}; 各要素の値を記載してください。
+		//答え：
+		int[] array2 = new int[] { 5, 6 };
+		System.out.println("array2[0]の値は 5 です。");
+		System.out.println("array2[1]の値は 6 です。");
+
+		//問題３︓int[][] array = new int[2][3]; 各要素の値を記載してください。
+		//答え：
+		int[][] array3 = new int[2][3];
+		System.out.println("array3の各要素の値は 0 です。");
+		array3[0] = new int[] { 8, 23, 12 };
+		array3[1] = new int[] { 35, 2, 45 };
+		for (int i = 0; i < array3.length; i++) {
+			for (int j = 0; j < array3[i].length; j++) {
+				System.out.println(array3[i][j]);
+			}
+		}
+
+		//問題４︓以下２重Foreachを理解してください。
+		int[][] array2d = new int[4][2];
+		for (int[] out : array2d) {
+			for (int inner : out) {
+				System.out.println(inner);
+			}
+		}
+		//答え：外部のForは array2dの4つ一次元配列によって、ループを実施する
+		//内部のForは outに設定した一次元配列の2つ項目によって、ループを実施する
+		//結果はarray2dの８つ要素の値を出力する
+
+		//問題５︓int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 左の配列の最大値と最小桁値をSystem.out.println()。
+		//答え：
+		int[] array4 = new int[] { 1, 3, 5, 7, 100, 0, 1 };
+		int max = array4[0];
+		int min = array4[0];
+		for (int item : array4) {
+			if (max < item) {
+				max = item;
+			}
+			if (min > item) {
+				min = item;
+			}
+		}
+		System.out.println("最大值：" + max);
+		System.out.println("最小值：" + min);
+
+		//問題６︓問題１の配列のSUMを求める。
+		//答え：
+		int sum = 0;
+		for (int item : array1) {
+			sum = sum + item;
+		}
+		System.out.println("array1のSUM値：" + sum);
+
+		//問題７︓行列（２次元配列）の和と積を計算してください。
+		//答え：
+		int a[][] = new int[][] {
+				{ 5, 12 },
+				{ 56, 3 }
+		};
+		int b[][] = new int[][] {
+				{ 45, 2 },
+				{ 52, 33 }
+		};
+		int c[][] = new int[2][2];
+
+		System.out.println("行列（２次元配列）の和：");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				c[i][j] += a[i][j] + b[i][j];
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("行列（２次元配列）の積：");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				c[i][j] = a[i][j] * b[i][j];
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
 }
--- src/co/jp/chapter05/JavaHomeWork05.java
+++ src/co/jp/chapter05/JavaHomeWork05.java
@@ -5,6 +5,127 @@ public class JavaHomeWork05 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	}
+		/** 05 新米８問 */
 
+		//設問１︓1から100までの偶数の和を求める(for + if)。
+		//答え：
+		int sum1 = 0;
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 == 0) {
+				sum1 = sum1 + i;
+			}
+		}
+		System.out.println("質問１、1から100までの偶数の和：" + sum1);
+
+		//設問２︓5の階乗を求める(for)。
+		//答え：
+		int a = 1;
+		for (int i = 1; i <= 5; i++) {
+			a = a * i;
+		}
+		System.out.println("質問２、5の階乗：" + a);
+
+		//質問３︓配列 {1, 2, 6, 7, 9, 6, 2, 1} の最大値、最小値、和、平均値を求める。
+		//答え：
+		int[] array1 = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+		int max = array1[0];
+		int min = array1[0];
+		int sum2 = 0;
+		for (int item : array1) {
+			if (max < item) {
+				max = item;
+			}
+			if (min > item) {
+				min = item;
+			}
+			sum2 = sum2 + item;
+		}
+		int ave = sum2 / array1.length;
+		System.out.println("質問３、配列 {1, 2, 6, 7, 9, 6, 2, 1} の");
+		System.out.println("最大值：" + max);
+		System.out.println("最小值：" + min);
+		System.out.println("和：" + sum2);
+		System.out.println("平均値：" + ave);
+
+		//質問４︓質問３の配列に重複ある要素を探す（要素の表示次数集計必要なし）。
+		//答え：
+		array1 = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+		System.out.println("質問４、array1に重複ある要素：");
+		for (int i = 0; i < array1.length - 1; i++) {
+			for (int j = i + 1; j < array1.length; j++) {
+				if (array1[i] == array1[j]) {
+					System.out.println(array1[i]);
+				}
+			}
+		}
+
+		//質問５︓質問３の配列に値「７」のインデックスを求む。
+		//答え：
+		array1 = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+		System.out.println("質問５、配列に値7のインデックス：");
+		for (int i = 0; i < array1.length; i++) {
+			if (array1[i] == 7) {
+				System.out.println(i);
+			} //重複の値「７」があるかもしれないので、breakしない
+		}
+
+		//質問６︓任意行列（２次元配列）の和を求める。
+		int[][] array21 = new int[][] {
+				{ 5, 12 },
+				{ 56, 3 }
+		};
+		int[][] array22 = new int[][] {
+				{ 45, 2 },
+				{ 52, 33 }
+		};
+		int total[][] = new int[2][2];
+
+		System.out.println("質問６、行列（２次元配列）の和：");
+		for (int i = 0; i < array21.length; i++) {
+			for (int j = 0; j < array21[i].length; j++) {
+				total[i][j] += array21[i][j] + array22[i][j];
+				System.out.print(total[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+		//質問７︓配列を昇順でソートする（java.util等メソッド利⽤禁⽌）。
+		//答え：
+		array1 = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+		for (int i = 0; i < array1.length - 1; i++) {
+			for (int j = 0; j < array1.length - 1 - i; j++) {
+				if (array1[j] > array1[j + 1]) {
+					int temp=array1[j];
+					array1[j]=array1[j+1];
+					array1[j+1]=temp;
+				}
+			}
+		}
+		System.out.print("質問７、昇順：");
+		for (int i = 0; i < array1.length; i++) {
+			System.out.print(array1[i]+" ");
+		}
+		System.out.println();
+
+		//質問８︓LV10のパスカルの三角形を出力してください。
+		//答え：
+		//LV10の2次元配列を宣言して、長さを設定する
+		int[][] array23=new int[10][];
+
+		System.out.println("質問８、LV10のパスカルの三角形：");
+	    for (int i = 0; i < array23.length; i++) {
+	      //第二階層の１次元配列を宣言して、長さを設定する
+	      array23[i]=new int[i+1];
+	      for(int j=0;j<=i;j++){
+	        //各行で最初と最後の要素の値を1に設定する
+	        if(j==0||j==i){
+	          array23[i][j]=1;
+	        }else{
+	          array23[i][j]=array23[i-1][j-1]+array23[i-1][j];
+	        }
+	        System.out.print(array23[i][j]+" ");
+	      }
+	      System.out.println();
+	    }
+	  }
 }
