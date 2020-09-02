6efacc251a62dfc9a7b71dc83a5ec543f071f7e8
USER <USER@192.168.1.106>
2020-09-02 19:57:52 +0900

    8/28 授業内容コミット

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -7,78 +7,138 @@ public class JavaChapter03 {
 		String str1 = "abc";
 		String str3 = new String("abc");
 
-//		int i = 10;// 変数 input
+		//		int i = 10;// 変数 input
 		// if - else if - else 開放区間
-//		if (i > 0 && i <= 9) {
-//			// ア：上記条件がtrueの場合、下記処理を実行
-//			System.out.println("変数.iはプラス");
-//		} else if (i < 0 && i >= -9) {
-//			// イ：ア条件がfalse かつ、上記条件がtrueの場合、
-//			// 下記処理を実行
-//			System.out.println("変数.iはマイナス");
-//		} else { // (i  ①<  ②= 0)
-//			// ウ：ア条件がfalse かつ、イ条件がfalseの場合、
-//			// 下記処理を実行
-//			// 上記以外
-//			System.out.println("変数.iはゼロ");
-//		}
+		//		if (i > 0 && i <= 9) {
+		//			// ア：上記条件がtrueの場合、下記処理を実行
+		//			System.out.println("変数.iはプラス");
+		//		} else if (i < 0 && i >= -9) {
+		//			// イ：ア条件がfalse かつ、上記条件がtrueの場合、
+		//			// 下記処理を実行
+		//			System.out.println("変数.iはマイナス");
+		//		} else { // (i  ①<  ②= 0)
+		//			// ウ：ア条件がfalse かつ、イ条件がfalseの場合、
+		//			// 下記処理を実行
+		//			// 上記以外
+		//			System.out.println("変数.iはゼロ");
+		//		}
 
-//		//
-//		if (i > 0 && i <= 9) { // １
-//			System.out.println("変数.iはプラス");
-//		} else if (i < 0 && i >= -9) { // 2
-//			System.out.println("変数.iはマイナス");
-//		} else if (i == 0) { // 3 ★★★★
-//			System.out.println("変数.iはゼロ");
-//		} else { //4
-//			System.out.println("input変数の値が計算対象外「-9～9」の間に指定しください");
-//		}
-//
-//		// switch 定数のみ byte short char int
-//		switch (i) {
-//		case 9:
-//		case 8:
-//		case 7:
-//		case 6:
-//		case 5:
-//		case 4:
-//		case 3:
-//		case 2:
-//		case 1:
-//			System.out.println("変数.iはプラス");
-//			break;
-//		case -9:
-//		case -8:
-//		case -7:
-//		case -6:
-//		case -5:
-//		case -4:
-//		case -3:
-//		case -2:
-//		case -1:
-//			System.out.println("変数.iはマイナス");
-//			break;
-//		case 0:
-//			System.out.println("変数.iはゼロ");
-//			break;
-//		default:
-//			System.out.println("input変数の値が計算対象外「-9～9」の間に指定しください");
-//		}
-
-
-
-		// loopの一つはFORループ
-		// 自然数 1 2 3 4 5
-		// 0 はＪＡＶＡの一番目の位置
+		//		//
+		//		if (i > 0 && i <= 9) { // １
+		//			System.out.println("変数.iはプラス");
+		//		} else if (i < 0 && i >= -9) { // 2
+		//			System.out.println("変数.iはマイナス");
+		//		} else if (i == 0) { // 3 ★★★★
+		//			System.out.println("変数.iはゼロ");
+		//		} else { //4
+		//			System.out.println("input変数の値が計算対象外「-9～9」の間に指定しください");
+		//		}
 		//
-		for (int i= 0 ; i< 10 ;i++) {
-			System.out.println(i);
-		}
-		for (int i= 0 ; i<= 10 ;i++) {
-			System.out.println(i);
-		}
+		//		// switch 定数のみ byte short char int
+		//		switch (i) {
+		//		case 9:
+		//		case 8:
+		//		case 7:
+		//		case 6:
+		//		case 5:
+		//		case 4:
+		//		case 3:
+		//		case 2:
+		//		case 1:
+		//			System.out.println("変数.iはプラス");
+		//			break;
+		//		case -9:
+		//		case -8:
+		//		case -7:
+		//		case -6:
+		//		case -5:
+		//		case -4:
+		//		case -3:
+		//		case -2:
+		//		case -1:
+		//			System.out.println("変数.iはマイナス");
+		//			break;
+		//		case 0:
+		//			System.out.println("変数.iはゼロ");
+		//			break;
+		//		default:
+		//			System.out.println("input変数の値が計算対象外「-9～9」の間に指定しください");
+		//		}
 
+		//		// loopの一つはFORループ
+		//		// 自然数 1 2 3 4 5
+		//		// 0 はＪＡＶＡの一番目の位置
+		//		for (int i= 0 ; i < 10 ;i++) {
+		//			if (i%2 != 0) {
+		//				continue;
+		//			}
+		//			System.out.println("FOR 10回　＝なし"+i);
+		//		}
+		//		System.out.println("ーーーーーーーーーーーーーーー");
+		//
+		//		// while
+		//		int j = 0; // dead loop
+		//		while (j > 10) {
+		//			System.out.println("while の LABALABA"+j);
+		//			j++;
+		//		}
+		//
+		//		// dead loop 下方のソースは → dead code（logic）　デットロジック
+		//		// {}の中に break 方のソースは → dead code（logic）　デットロジック
+		//		// if (true) のelse にも →  dead code（logic）　デットロジック
+		//
+		//		System.out.println("ーーーーーーーーーーーーーーー");
+		//		// do while
+		//		int x = 0;
+		//		do {
+		//			System.out.println("do while の LABALABA  "+ x );
+		//			x++;
+		//		} while (x > 10);
+		//
+		//
+		//
+		//		System.out.println("ーーーーーーーーーーーーーーー");
+		//
+		//
+		//
+		//		for (int i= 0 ; i<= 10 ;i++) {
+		//			System.out.println("FOR 10回　＝あり"+i);
+		//		}
+		//		System.out.println("ーーーーーーーーーーーーーーー");
+		//		// while
+		//		int j1 = 0; // dead loop
+		//		while (j1 <= 10) {
+		//			System.out.println("while の LABALABA"+j1);
+		//			j1++;
+		//		}
+		//		// do while
 
+		//		for (int i=0;i<25;i++) {
+		//			System.out.print("1");
+		//			if (i>0 && (i+1)%5==0) {
+		//				System.out.println();
+		//			}
+		//		}
+
+		//		int x = 8;
+		//		for (int i = 0; i < (x * x); i++) {
+		//			System.out.print("1");
+		//			if (i > 0 && (i + 1) % x == 0) {
+		//				System.out.println();
+		//			}
+		//		}
+
+		int x = 100;
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < x; j++) {
+				if (i == j || i + j == x - 1) {
+					System.out.print(i + "" + j + " ");
+				} else {
+					System.out.print("   ");
+				}
+			}
+			System.out.println();
+		}
 
 	}
 
--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -5,6 +5,41 @@ public class JavaChapter04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		// 配列 (数組)
+		int a = 0;
+
+		// 整形配列
+		int[] array = new int[] { 1, 2, 3, 4, 5 };
+		// 文字列配列
+		String[] strArray = new String[] { "1", "2", "3", "4", "5" };
+
+		//
+		Object[] objArray = new Object[] { 1, "333", 1.1, 1f, true };
+
+		// 定義方式① 宣言   かつ   初期化時値を設定
+		int[] intArray_1 = new int[] { 55, 22,33,11, 22, 555 };
+		for (int index = 0; index < intArray_1.length; index++) {
+			System.out.println(intArray_1[index]);
+		}
+
+
+		int[] intArray_2 = new int[] { 1, 2,3,4, 5, 565 };
+
+		for (int index = 0; index < intArray_2.length; index++) {
+			System.out.println(intArray_2[index]);
+		}
+
+		//  定義方式② 宣言 かつ 長さ定義のみ
+		int[] intArray_3 = new int[4];
+		for (int index = 0; index < intArray_3.length; index++) {
+			System.out.println(intArray_3[index]);
+		}
+
+		Integer[] intArray_4 = new Integer[4];
+		for (int index = 0; index < intArray_4.length; index++) {
+			System.out.println(intArray_4[index]);
+		}
+
 	}
 
 }
