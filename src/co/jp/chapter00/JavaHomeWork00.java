9c10c7f064c170b97ef32ddb067041b6e7f8d20e
admin <admin@LAPTOP-PB4SIJJC>
2020-09-02 20:09:03 +0900

    20200902_Chapter00_ROKENKA

---
+++ src/co/jp/chapter00/JavaHomeWork00.java
@@ -0,0 +1,12 @@
+package co.jp.chapter00;
+
+public class JavaHomeWork00 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//現在現場システムテストレビューやっています
+		//VB.NETとJAVAScrip
+	}
+
+}
