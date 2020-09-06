ac78be92821100eeba934eb614786a7b2aea357d
ZhangWeilu <qiuqiu-zhang36@hotmail.com>
2020-09-06 20:39:55 +0800

    JavaHomeWork01_ZWL

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,29 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+//Ｐ３の質問：変数 x と y の値を回答してください。
+//変数 x  = 20 ; 変数 y = 30；
+
+
+//Ｐ６の質問１：以下変数ｎの値は
+// int i = 10;
+// int n = i%5;
+// System.out.println("int n = "+ n);
+//答：ｎ＝０
+
+
+//Ｐ６の質問２：a = a > 10 ? 11 : 0
+//a > 10の場合に、aの値は11を返し、
+//逆には0を返します。
+
+
+//Ｐ６の質問３：
+//変数値を比較する演算子は‟＝＝”です。
+
+
+//Ｐ６の質問４：
+//boolean変数に設定可能の値は‟true と false”です。
+
 	}
 
 }
