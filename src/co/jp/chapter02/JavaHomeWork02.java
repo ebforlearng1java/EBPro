8e37580b0c8a1e13945823366a2b1457719aac22
Qiuqiu <Qiuqiu@VAIO>
2020-09-06 21:00:07 +0800

    ０２型（文字列）の宿題

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -5,6 +5,31 @@ public class JavaHomeWork02 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+//質問１：System.out.println(“this is line1\tthis line2“); は何行出力しますか？
+// 1.１行
+
+
+//質問２：Stringを比較方法は？
+// 2.equals
+
+
+//質問３：Float値⇒String文字列変更する場合、どうなメソッドを利用しますか
+//1.String.valueOf(‟1.0F‟)
+
+
+//質問４：以下式の値を記載してください
+//1. 1.0+2.0=3.0
+//2. 1.0+‟2.0‟の場合に：
+		System.out.println(1.0 + 2.0);
+		System.out.println(1.0 + "2.0");
+//1.0+‟2.0‟=1.02.0
+
+
+//質問５：以下ステートメントの出力を記載してください
+		int a = 10;
+		System.out.println("a");
+//出力はaです
+
 	}
 
 }
