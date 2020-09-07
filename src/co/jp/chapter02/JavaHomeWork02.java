212a7944b186ab454ffac5075a0a25e4bbd5e21b
zyc <zyc@DESKTOP-H0B0IIC>
2020-09-07 22:57:53 +0900

    homework02_0907趙雨晨

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -11,28 +11,33 @@ public class JavaHomeWork02 {
 		//「質問４」3.0;"1.02.0"
 		//「質問５」a
 
+		//質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
 		System.out.println("質問1");
 		System.out.println("2行");
 		System.out.println();
-		
+
+		//質問２︓Stringを比較方法は︖
 		System.out.println("質問2");
 		System.out.println("equals");
 		System.out.println();
-		
+
+		//質問３︓Float値⇒String文字列変更する場合、どうなメソッドを利⽤しますか
 		System.out.println("質問3");
 		System.out.println("String str = String.valueOf(\"1.0F\")");
 		System.out.println();
-		
+
+		//質問４︓以下式の値を記載してください。
 		System.out.println("質問4");
 		System.out.println("3.0;\"1.02.0\"");
 		System.out.println();
-		
+
+		//質問５︓以下ステートメントの出力を記載してください。
 		System.out.println("質問5");
 		System.out.println("a");
 		System.out.println();
-		
-		
-		
+
+
+
 	}
 
 }
