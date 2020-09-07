df7b3410af82d4faa6dd7d4126839f2d140acfb2
admin <admin@DESKTOP-FU4R8QJ>
2020-09-07 20:28:03 +0800

    宿題コミット（9/7）

---
+++ src/co/jp/chapter00/JavaHomeWork00.java
@@ -0,0 +1,16 @@
+package co.jp.chapter00;
+
+public class JavaHomeWork00 {
+
+	public static void main(String[] args) {
+
+	  // 質問１　今現場に自分担当のフェーズと成果物
+	  // 担当のフェーズは基本設計の改修です。
+	  // 成果物は画面レイアウト設計書、チェック編集仕様書
+
+	  // 質問２　今現場に使用しているIDE　（開発ツール）
+	  // 開発言語向け Java（eclipse）
+	  // DB向け : DB名（HIRDB SQL Executer）（日立自用）
+	}
+
+}
