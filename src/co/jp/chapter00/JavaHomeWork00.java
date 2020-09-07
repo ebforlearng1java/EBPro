88d688f7a5e4ad7a3798cef27cf7b198e6c58a22
user <user@192.168.0.104>
2020-09-07 23:33:51 +0900

    喬2020/09/07

---
+++ src/co/jp/chapter00/JavaHomeWork00.java
@@ -0,0 +1,53 @@
+package co.jp.chapter00;
+
+public class JavaHomeWork00 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		// 質問１　今現場に自分担当のフェーズと成果物
+		// 答え　自動車保険の保守案件でテストを担当いている。
+		//       成果物：UT,Ita,Itb,ST
+
+		// 質問2　現場に使用しているDE　（開発ツール）
+		  // 開発言語向け　　back：VB;　　front：JS、HTML
+		  // DB向け:SQLサーバ2008、DB2；
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
