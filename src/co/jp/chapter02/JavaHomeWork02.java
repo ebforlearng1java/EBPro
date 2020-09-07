ff2895ad2e2927a38aa6c97f14b933f1bab89c02
zyc <zyc@DESKTOP-H0B0IIC>
2020-09-07 22:21:39 +0900

    homework_趙

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -5,6 +5,34 @@ public class JavaHomeWork02 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//「質問１」2行
+		//「質問２」equals
+		//「質問３」String str = String.valueOf("1.0F")
+		//「質問４」3.0;"1.02.0"
+		//「質問５」a
+
+		System.out.println("質問1");
+		System.out.println("2行");
+		System.out.println();
+		
+		System.out.println("質問2");
+		System.out.println("equals");
+		System.out.println();
+		
+		System.out.println("質問3");
+		System.out.println("String str = String.valueOf(\"1.0F\")");
+		System.out.println();
+		
+		System.out.println("質問4");
+		System.out.println("3.0;\"1.02.0\"");
+		System.out.println();
+		
+		System.out.println("質問5");
+		System.out.println("a");
+		System.out.println();
+		
+		
+		
 	}
 
 }
