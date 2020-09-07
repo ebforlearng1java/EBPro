8c66748516597abd4c488ef1b311cadd670a5ca8
zyc <zyc@DESKTOP-H0B0IIC>
2020-09-07 22:19:41 +0900

    homework01_趙

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,34 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//「質問１」ｎの値は0
+		//「質問２」aは10より大きいですか。trueの場合11を返す。falseの場合0を返す
+		//「質問３」==
+		//「質問４」true,false
+		//「質問５」xの値は20、ｙの値は30
+		System.out.println("質問1");
+		System.out.println("ｎの値は0");
+		System.out.println();
+		
+		System.out.println("質問2");
+		System.out.println("aは10より大きいですか。trueの場合11を返す。falseの場合0を返す");
+		System.out.println();
+		
+		System.out.println("質問3");
+		System.out.println("==");
+		System.out.println();
+		
+		System.out.println("質問4");
+		System.out.println("true,false");
+		System.out.println();
+		
+		System.out.println("質問5");
+		System.out.println("xの値は20、ｙの値は30");
+		System.out.println();
+		
+
+
+
 	}
 
 }
