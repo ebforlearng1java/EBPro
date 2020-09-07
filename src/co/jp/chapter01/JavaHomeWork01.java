21bed728314bcfd1caa61412b21104270d74c4f3
zyc <zyc@DESKTOP-H0B0IIC>
2020-09-07 22:57:11 +0900

    homework01_0907趙雨晨

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,31 +5,34 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		//「質問１」ｎの値は0
-		//「質問２」aは10より大きいですか。trueの場合11を返す。falseの場合0を返す
-		//「質問３」==
-		//「質問４」true,false
-		//「質問５」xの値は20、ｙの値は30
+
+		//質問１︓以下変数nの値は︖
+		//1int i = 10;
+		//2int n = i%5;
 		System.out.println("質問1");
 		System.out.println("ｎの値は0");
 		System.out.println();
-		
+
+		//質問2︓右の式を解釈してください: a = a > 10 ? 11 : 0;
 		System.out.println("質問2");
 		System.out.println("aは10より大きいですか。trueの場合11を返す。falseの場合0を返す");
 		System.out.println();
-		
+
+		//質問３︓変数値を⽐較する演算⼦は = or == ?
 		System.out.println("質問3");
 		System.out.println("==");
 		System.out.println();
-		
+
+		//質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
 		System.out.println("質問4");
 		System.out.println("true,false");
 		System.out.println();
-		
+
+		//下記コードで、変数x とy の値を回答してください。
 		System.out.println("質問5");
 		System.out.println("xの値は20、ｙの値は30");
 		System.out.println();
-		
+
 
 
 
