ca289f756845a0fac7f65cabbf5a01ffa587d316
13254 <13254@DESKTOP-08SP42E>
2020-09-08 22:44:48 +0900

    00 KNZW

---
+++ src/co/jp/chapter07/JavaChapter07.java
@@ -0,0 +1,15 @@
+package co.jp.chapter07;
+
+public class JavaChapter07 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		boolean flg = true;
+		System.out.println("questiong1");
+		int i = 10;
+		int n = i % 5;
+		System.out.println("nの値"+n);
+	}
+
+}
---
+++ src/co/jp/chapter07/JavaHomeWork09.java
@@ -0,0 +1,15 @@
+package co.jp.chapter07;
+
+public class JavaHomeWork09 {
+
+	public static void main(String[] args) {
+		//00の質問1
+		//今現場に自分担当のフェーズと成果物
+
+		//答え：ITa_入力チェック（クライアント）
+		//今現場に使用しているIDE
+		//答え　VB
+
+	}
+
+}
