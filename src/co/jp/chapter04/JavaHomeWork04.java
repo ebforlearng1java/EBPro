eaaed8d2e9c61dc4d1fb040fb13abd5303581166
Qiuqiu <Qiuqiu@VAIO>
2020-09-08 19:15:38 +0800

    ０４＿配列の宿題

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -5,6 +5,75 @@ public class JavaHomeWork04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+//質問１：
+//		int[] Array_01= new int[4];
+//		for(int i=0;i<Array_01.length;i++) {
+//		System.out.println(Array_01[i]); //配列の要素を出力する
+//		}
+//答：Array[0]=0;Array[1]=0;Array[2]=0;Array[3]=0
+
+
+////質問２:
+//		int[] Array_02 = new int[] {5,6};
+//		for(int i=0;i<Array_02.length;i++) {
+//			System.out.println(Array_02[i]); //配列の要素を出力する
+//		}
+////答：Array[0]=5;Array[1]=6
+
+
+//質問３：
+// array[0][0] = 0;
+// array[0][1] = 0;
+// array[0][2] = 0;
+// array[1][0] = 0;
+// array[1][1] = 0;
+// array[1][2] = 0;
+
+
+//質問４：
+//外部のforは、array2dを４つの配列を出力する
+//内部のforは、outによって設定したinnerを繰り返し出力する
+//最後に8つの値を出力する
+
+
+//質問５：
+//		int chengji;
+//		double sum = 0;
+//		int[] array=new int[] {1,3,5,7,100,0,1};
+//		int max = array[0];
+//		int min = array[0];
+//// int i ,b;
+//		for(int i=0;i<array.length-1;i++) {
+//			if(max<array[i]) {
+//				max = array[i];
+//			}
+//			if(min>array[i]) {
+//				min = array[i];
+//			}
+//		}
+//		System.out.println("最大値"+ max);
+//		System.out.println("最小値"+ min);
+//答：最大値100;最小値0
+
+
+//質問６：
+//		int[] array_03 = new int [4];
+//		System.out.println(array_03[0]+array_03[1]+array_03[2]+array_03[3]);
+//答：SUMの値は０です
+
+
+//質問７：
+//		int[][] array = {
+//				{1,2,3,4},
+//				{10,20,30,40,50,60},
+//				{1000,10000}
+//		};
+//		int sum = 0;
+//		for (int i=0; i<array.length; i++) {
+//			for(int s=0; s<array[i].length; s++) {
+//				sum += array[i][s];
+//			}
+//		}
 	}
 
 }
