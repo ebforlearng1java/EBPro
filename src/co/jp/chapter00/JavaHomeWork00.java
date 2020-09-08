5864a38401dd51ecb52a305895cd6c1dfd444f3a
andyh <andyh@DESKTOP-V0EV2LH>
2020-09-08 22:29:40 +0900

    2020/09/08

--- src/co/jp/chapter00/JavaHomeWork00.java
+++ src/co/jp/chapter00/JavaHomeWork00.java
@@ -6,7 +6,8 @@ public class JavaHomeWork00 {
 		// TODO 自動生成されたメソッド・スタブ
         //私は基本的に運用保守の仕事をやります。
 
-		System.out.println("私は現場で基本設計から運用保守まで一連の作業をやります。");
+		System.out.println("私は現場で基本設計から運用保守まで一連の作業を担当します。");
+		System.out.println("成果物は多様です。");
 	}
 
 }
