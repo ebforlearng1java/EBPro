7881a70f42bc1710c8fbd34ca87bfce909456960
andyh <andyh@DESKTOP-V0EV2LH>
2020-09-08 22:26:04 +0900

    2020/09/08 宿題提出

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -7,6 +7,8 @@ public class JavaHomeWork04 {
 
 
 		//質問1
+		System.out.println();
+		System.out.println("質問1");
 		int[] arr1 = new int[4];
 		for(int i=0;i<arr1.length;i++) {
 			arr1[i] = i+1;
@@ -14,12 +16,16 @@ public class JavaHomeWork04 {
 		}
 
 		//質問2
+		System.out.println();
+		System.out.println("質問2");
 		int[] arr2 = new int[] {5,6};
 		for(int j=0;j<arr2.length;j++) {
 			System.out.println(arr2[j]);
 		}
 
 		//質問3
+		System.out.println();
+		System.out.println("質問3");
 		int[][] arr3 = new int[2][3];
 		for(int i=0;i<arr3.length;i++) {
 
@@ -32,16 +38,28 @@ public class JavaHomeWork04 {
 
 
 		//質問4
+		//以下ソースを理解する。
+		System.out.println();
+		System.out.println("質問4");
+		System.out.println("説明はソースに入れるコメントを参照してください");
+		//arr4という二次元配列を新規する。二つ要素を持つ一次元配列四つをまとめて、二次元配列となる。
 		int [][] arr4 = new int[4][2];
+		//配列arr4の中に、outという二つ要素を持つ一次元配列が存在する。
 		for(int[] out:arr4) {
+			//outという二つ要素を持つ一次元配列の中に、innerという要素が存在する。
 			for(int inner : out) {
+				//innerという要素を出力してください。
+				//実は、innerでもoutでも、具体的な値を紐づけないので、すべての要素は順番に出力される。
 				System.out.println(inner);
+				//ただ、この配列は値を付与されないので、0しか出力しない。
 			}
 		}
 
 
 	    //質問5
 		//最大値と最小値を求める
+		System.out.println();
+		System.out.println("質問5");
 		int[] arr5 = new int[] {1,3,5,7,100,0,1};
 		int max = arr5[0];
 		int min = arr5[0];
@@ -59,6 +77,8 @@ public class JavaHomeWork04 {
 
 		//質問6
 		//質問1配列の和を求める
+		System.out.println();
+		System.out.println("質問6");
 		int sum6 = 0;
 		for(int i=0;i<arr1.length;i++) {
 			sum6 = sum6+arr1[i];
@@ -68,16 +88,18 @@ public class JavaHomeWork04 {
 		//質問7
 		//二次元配列の和と積を計算
 		//和を求める
+		System.out.println();
+		System.out.println("質問7");
 		int[][] arr7 = new int[][] {
 			{1,2,3},{4,5,6}
 		};
 		int sum7 = 0;
 		for(int a=0;a<arr7.length;a++) {
 			for(int b=0;b<arr7[a].length;b++) {
-				System.out.println(arr7[a][b]+" ");
+				//System.out.println(arr7[a][b]+" ");
 				sum7 = sum7 + arr7[a][b];
 			}
-			System.out.println();
+			//System.out.println();
 		}
 		System.out.println("この二次元配列の和は"+sum7+"です。");
 
