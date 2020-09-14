a7590b4040c433a0ce502ea954fc9f08bf7cbde7
xu <xu@192.168.0.13>
2020-09-15 00:50:52 +0900

    09/15 chapter06 1～5 宿題

--- src/co/jp/chapter06/JavaHomeWork06.java
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -16,12 +16,52 @@ public class JavaHomeWork06 {
 
 		//質問２．配列の和、MAX値、MIN値を求める処理を関数化してください、main関数に呼び出してください。
 
+        	int [] arr = new int [] {20,40,10,40};
+        	int maxTest = max(arr);
+        	int minTest = min(arr);
+        	int sumTest = sum(arr);
+
+        	System.out.println(maxTest);
+        	System.out.println(minTest);
+        	System.out.println(sumTest);
+        }
 
 
+        public static int max(int [] datas)
+        {
+        	int max = datas[0];
+        	for (int i = 1; i < datas.length; i++) {
+        		int v =datas[i];
+        		if (v> max) {
+        			max = v;
+
+        		}
+        	}
+        	return max;
+        }
 
 
+        public static int min(int [] datas)
+           {
+            int min = datas[1];
+            for (int i = 1; i < datas.length; i++) {
+            	int v =datas[i];
+            	if (v< min) {
+            		min = v;
 
+            	}
+            }
+           	return min;
+        }
 
+        public static int sum(int [] datas)
+        {
+         int sum = datas[0];
+         for (int i = 1; i < datas.length; i++) {
+         	sum = sum + datas[0];
+
+         	}
+        	return sum;
 
 
 
