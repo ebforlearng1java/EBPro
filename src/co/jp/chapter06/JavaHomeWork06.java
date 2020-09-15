c4840bb32c4315fc996508ce1b2050c63dcb71b7
Qiuqiu <Qiuqiu@VAIO>
2020-09-15 22:09:03 +0800

    ０６宿題１－５

--- src/co/jp/chapter06/JavaHomeWork06.java
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -5,6 +5,35 @@ public class JavaHomeWork06 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	}
 
+		//質問１：
+		//①.修飾子は: public static
+		//②.返却型は: void
+		//③.関数名は: main
+		//④.引数（引数の型）は: String[]
+		//⑤.引数（引数の名称）は: args
+
+
+		//質問３：
+		//	private static void sayHello(String y) {
+		//		y = "Hello world";
+		//	}
+		//上記メソッドの実施結果はNullです
+
+
+		//質問４：
+		// private static void fillArray(String[] array) {
+		// array[0] = "value2";
+		// }
+		//上記メソッドの実施結果は"value2"です
+
+
+		//質問５：
+		// private static void fillArray(String[] array) {
+		// array = new String[] {"1", "2"};
+		// }
+		//上記メソッドの実施結果はTRUEです
+
+
+	}
 }
