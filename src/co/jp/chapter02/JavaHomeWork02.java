98e79bbd1d92f4ea286acc955b46edb87f68fd73
admin <admin@LAPTOP-PB4SIJJC>
2020-09-16 20:07:44 +0900

    20200916_コミット漏れ

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -4,10 +4,18 @@ public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-		
-		
-		
+		//質問１:1行
+		//質問２:equals
+		//質問３:2. 1.0F + ““
+		//質問４:1.0 + 2.0 = 3.0
+		//       1.0 + "2.0" = 1.02.0
+		//質問５:a
+		public static void main(String[] args) {
+            int a = 10;
+            System.out.println("a");
+            }
+
+
 
 	}
 
