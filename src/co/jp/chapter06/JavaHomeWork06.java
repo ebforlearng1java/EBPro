d71d5e56e351c59ab019a1edac249a870066800b
admin <admin@LAPTOP-PB4SIJJC>
2020-09-16 16:14:31 +0900

    20200916_Chapter06_rokenka

--- src/co/jp/chapter06/JavaHomeWork06.java
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -4,6 +4,155 @@ public class JavaHomeWork06 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問１．以下関数の修飾⼦、返却型、関数名、引数（引数の型・名称）を回答してください。
+		//public static int max(String[] args);
+		//修飾子：public static
+		//返却型：int
+		//関数名：max
+		//引数：引数の型String,名称args
+
+		//質問２.配列の和、MAX値、MIN値を求める処理を関数化してください、main関数に呼び出してください。
+//		public static int max(int[] arr) {
+//			int max;
+//			if (null != arr && arr.length > 0) {
+//				max = arr[0];
+//
+//				for (int x : arr) {
+//					if (x > max)
+//						max = x;
+//				}
+//				System.out.println(max);
+//				return max;
+//
+//			} else {
+//				return 0;
+//			}
+//		}
+//		public static int min(int[] arr) {
+//			int min;
+//			if (null != arr && arr.length > 0) {
+//				min = arr[0];
+//
+//				for (int x : arr) {
+//					if (x < min)
+//						min = x;
+//				}
+//				System.out.println(min);
+//				return min;
+//			} else {
+//				return 0;
+//			}
+//		}
+//		public static int sum(int[] arr) {
+//			int sum = 0;
+//			for (int x : arr) {
+//				sum += x;
+//			}
+//			System.out.println(sum);
+//			return sum;
+//		}
+
+		//質問３.以下メソッドの実施結果を教えてください。
+		//実施結果はnullと思う。
+
+		//質問４.以下メソッドの実施結果を教えてください。
+		//実施結果はvalue2と思う。
+
+		//質問５.以下メソッドの実施結果を教えてください。
+		//実施結果はtrueと思う。
+		
+		//質問６-０:配列は直接に印刷できません。下記メソッドを利用して、
+		//配列をprintします。以下メソッドを呼び出してください。
+
+
+
+
+
+
+
+
+
+
+
+
+		//質問６-１:以下静的なメソッドを定義してください。
+		//1. メソッド名: megerArray
+		//2. 引数１︓intの配列 left
+		//3. 引数２︓intの配列 right
+		//4. 戻り値︓intの配列
+		//5. 処理内容︓引数left と 引数right をマージして、新しい配列を作成して返却する。
+
+
+
+
+
+
+
+
+
+		//質問６-２:以下静的なメソッドを定義してください。
+		//1. メソッド名: subArray
+		//2. 引数１︓intの配列 array
+		//3. 引数２︓開始のstartIndex
+		//4. 引数３︓⻑さlength
+		//5. 戻り値︓intの配列
+		//6. 処理内容︓startIndexからstartIndex + lengthまでサブ配列を取得する
+		//以下メソッドの実施例:
+
+
+
+
+
+
+
+
+
+		//質問７︓以下静的なメソッドを定義してください。
+		//1. メソッド名:trim
+		//2. 引数１︓charの配列
+		//3. 戻り値︓charの配列
+		//4. 処理内容︓引数１のcharの配列先頭と末尾の空⽩⽂字列を削除してください。
+		//以下メソッドの実施例:
+
+
+
+
+
+
+
+
+
+		//質問８︓バブルソート⽤メソッドを作成してください。
+		//1. メソッド名︓bubbleSort
+		//2. 引数１︓intの配列
+		//3. 戻り値︓ソート済みの配列
+		//4. 処理内容︓bubbleSortアルゴリズムを用い、入力した引数の配列をソートしてください。
+		static int[] bubbleSort(int[] arr) {
+			for (int i = 0; i < arr.length - 1; i++) {
+				for (int j = 0; j < arr.length - 1 - i; j++) {
+					if (arr[j] > arr[j + 1]) {
+						int temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+					}
+				}
+			}
+			return arr;
+		}
+		
+		
+		//質問９︓メソッドの呼び出す練習。
+		//1. 質問６-１を用い、２つ配列をマージします。
+		//2. 質問８を用い、ステップ１のマージした配列をソートする。
+		//3. 注意:コードを１行にしてください。
+
+
+
+
+
+
+
+
 
 	}
 
