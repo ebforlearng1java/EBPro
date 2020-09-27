758f3cc5234ab1264ca10001d628bb57e3692cd8
andyh <andyh@DESKTOP-V0EV2LH>
2020-09-27 17:36:34 +0900

    2020/09/27

--- src/co/jp/chapter12/JavaHomeWork12.java
+++ src/co/jp/chapter12/JavaHomeWork12.java
@@ -5,6 +5,26 @@ public class JavaHomeWork12 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//質問1
+		int[] arr1 = new int[] {1,2,3};
+		try{
+			  for (int i = 0; i < 4; i++){
+			    System.out.println(arr1[i]);
+			  }
+			}
+			catch(ArrayIndexOutOfBoundsException e){
+			  System.out.println("配列の範囲を超えています");
+			}
+
+
+		//質問2
+
+
 	}
+
+
+
+
+
 
 }
