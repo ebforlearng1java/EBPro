db9883b3a027ec8806fb00782b7afdd7274be9a7
user <user@192.168.0.103>
2020-09-29 23:30:09 +0900

    2020/09/29 喬

--- src/co/jp/chapter07/JavaHomeWork07.java
+++ src/co/jp/chapter07/JavaHomeWork07.java
@@ -4,7 +4,36 @@ public class JavaHomeWork07 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		//質問１︓以下コードのコンパイルエラー理由を教えてください。
+		// Pet.java
+//		public class Pet {
+//		    public String name;
+//		    
+//		    public void setName(String value) {		    	
+//		        this.name = value;		        
+//		    }
+//		}		
+		// Kicker.java
+//		public class Kicker {		
+//		    public static void main(String...args) {		    	
+//		        Pet cat = new Pet();		        
+//		        cat.setAge(1234);
+//		        
+//		    }
+//		}
+		//答え：final が修飾された変数は１回のみ値変更可能です
+		
+	       //質問２︓Mathクラスを作成してください。以下メソッドを定義します。
+		
+		   //1. 配列のMAXを求める静的メソッドを定義。（引数は int 配列）
+		   //2. 配列のMINを求める静的メソッドを定義。（引数は int 配列）
+		   //3. 配列の平均値を求める静的メソッドを定義。（引数は int 配列）
+		   //4. mainメソッドで上記それぞれメソッドを呼び出してください。
+		   //同じパッケージのMath クラス
 
 	}
 
 }
+ 
