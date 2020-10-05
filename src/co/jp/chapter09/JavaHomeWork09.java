128d08b9517297ac892e45a117643ebd827ac64e
Everbe <a410526712@yahoo.co.jp>
2020-10-05 21:37:52 +0900

    OOP - 練習問題

--- src/co/jp/chapter09/JavaHomeWork09.java
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -38,6 +38,54 @@ public class JavaHomeWork09 {
 //			}
 		System.out.println("4行目");
 		
+		//質問１︓クラス（Class）とは︖クラス定義⽤のキーワードは「class 」である。
+		System.out.println("クラス定義⽤のキーワードは「class 」である");
+		
+//		質問２︓クラスメンバ（Class member）の何種類ありますか。
+		System.out.println("filed function method operator");
+		
+//		質問３︓クラスメンバの関数は特殊の関数「コンストラクタ」が持っています。
+		System.out.println("コンストラクタは public JavaHomeWork09(){}");
+		
+		
+//		質問４︓コンストラクタの特徴
+		System.out.println("1. 戻り値不要\r\n" + 
+				"2. 関数名＝クラス名称");
+		
+//		質問５︓コンストラクタの作⽤︓クラス属性の初期化する。
+		System.out.println("クラス属性の初期化する。");
+		
+//		質問６︓１つクラスに複数のコンストラクタ持つ可能。
+		System.out.println("例：public JavaHomeWork09(){}\r\n"+"public JavaHomeWork09(String name){}");
+		
+//		質問７︓キーワードthis の使⽤⽅法︓ this.name ︖ name ?
+		System.out.println("this.name = name;");
+		
+//		質問８︓クラスを利⽤する為、インスタンス化する必要です。インスタンスの演算⼦は
+//		new です。
+		System.out.println("例 ：JavaHomeWork09 newInstance = new JavaHomeWork09();");
+		
+//		質問９︓演算⼦new の直後呼び出しているメソッドは「コンストラクタ」︔
+		System.out.println("People obj = new People();");
+		
+//		質問１０︓アクセス制御の３種類あります。
+		System.out.println("1. + public\r\n" + 
+				"2. # protected\r\n" + 
+				"3. - private");
+		
+//		質問１１︓静的のメンバの定義するキーワード（修飾⼦）はstatic 。
+		System.out.println("static");
+		
+//		質問１２︓静的のメンバアクセス時、インスタンス化不要。アクセス⽅法は「クラス名.
+//		静的のメンバ」。
+		System.out.println(
+				"System.out.println(\"1234\"); // 静的のField\r\n" + 
+				"String value = String.format(\"%05d\", 0); // 静的のメソッド");
+		
+//		質問１３︓クラス「Animal」を定義して、インスタンス化しオブジェクト cat を作成しま
+//		す。変数catのsayHello()メソッドを呼び出してください。
+		
+		
 
 	}
 
