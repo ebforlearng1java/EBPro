d0147cae526dff10b39e6f382c7e0b11faac2c70
Everbe <a410526712@yahoo.co.jp>
2020-10-05 21:13:46 +0900

    OOP - その３

--- src/co/jp/chapter09/JavaHomeWork09.java
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -4,6 +4,40 @@ public class JavaHomeWork09 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+//		質問１︓以下ソースにラムダ式の使う場所を解釈してください。
+//		import java.awt.event.ActionEvent;
+//		import java.awt.event.ActionListener;
+//		import javax.swing.JButton;
+//		import javax.swing.JFrame;
+//		public class GUISample {
+//		public static void main(String[] args) {
+//		JFrame window = new JFrame("DCNet Java 教育");
+//		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		window.setSize(800, 600);
+//		JButton btn = new JButton("hello world");
+//		window.getContentPane().add(btn);
+//		btn.addActionListener((ActionEvent e) -> {
+//		System.out.println("ボタンクリックしました。");
+//		});
+//		window.setVisible(true);
+//		}
+//		}
+		System.out.println("btn.addActionListener(ActionEvent e) = new ActionListener()");
+		
+//		質問２︓以下ソースに匿名クラスの利⽤する⾏は︖
+//		public class Kicker {
+//			public static void main(String... args) {
+//			new Thread() {
+//			@Override
+//			public void run() {
+//			System.out.println("thread running...");
+//			}
+//			}.start();
+//			}
+//			}
+		System.out.println("4行目");
+		
 
 	}
 
