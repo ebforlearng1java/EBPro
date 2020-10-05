b6a3f96c2e57773c0ddde667b00fb548cf115d2f
user <user@DESKTOP-3R1LK91>
2020-10-05 22:20:17 +0900

    2020/10/05

--- src/co/jp/chapter08/JavaHomeWork08.java
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -4,7 +4,7 @@ public class JavaHomeWork08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 		//質問１：以下コードの出力するを回答してください。
 		String x = "a";
 		String y = "a";
@@ -12,11 +12,11 @@ public class JavaHomeWork08 {
 		//答え：false； 原因：メモリアドレスは一致しません。
 		System.out.println(x.equals(y)); // true ? false? 原因は ?
 		//答え：true； 原因：文字列内容は一致します。
-		String empty = "";
-		String nullString = null;
+//		String empty = "";
+//		String nullString = null;
 		// System.out.println(nullString.equals(empty)); // true ? false? 原因は ?
 		//答え：false; 原因：emptyはobject対象がある。nullStringはobject対象はない。
-		
+
 		//質問２:以下コードのしゅつを回答してください。
 		//同じパッケージのEmployeeクラスとKickerクラスを参照してください。
 
