61bd68a52ae358d59ced106d1ea3a4941d6db857
Qiuqiu <Qiuqiu@VAIO>
2020-10-06 21:54:42 +0800

    宿題０６

--- src/co/jp/chapter06/JavaHomeWork06.java
+++ src/co/jp/chapter06/JavaHomeWork06.java
@@ -14,6 +14,13 @@ public class JavaHomeWork06 {
 		//⑤.引数（引数の名称）は: args
 
 
+		//質問２：
+//		int[] array = new int[] { 10, 6, 5, 4, 5, 2 };
+// 		System.out.println("配列の和：" + sum(array));
+//		System.out.println("配列のMAX値：" + max(array));
+//		System.out.println("配列のMIN値：" + min(array));
+
+
 		//質問３：
 		//	private static void sayHello(String y) {
 		//		y = "Hello world";
@@ -35,5 +42,75 @@ public class JavaHomeWork06 {
 		//上記メソッドの実施結果はTRUEです
 
 
-	}
+		//質問６－０：
+		//String[] objs = new String[] { "1", "2", "3", "4", "5" };
+		//printArray(objs);
+
+
+		//質問６ー１：
+		//int[] left = new int[] {1,2,3,4};
+		//int[] right = new int[] {5,6,7,8};
+		//System.out.println(Arrays.toString(contact(left, right)));
+	//}
+	//public static int[] contact(int[] a, int[] b) {
+		//int[] result = new int[a.length + b.length];
+		//for (int i = 0; i < result.length; i++) {
+			//if (i < a.length) {
+				//result[i] = a[i];
+			//}else {
+				//result[i] = b[i - a.length];
+
+			//}
+		//}
+		//return result;
+
+
+		//質問６－２：
+		//int[] arr = {1,2,3,4};
+		//int[] new_arr = Arrays.copyOfRange(arr, 1, 3);
+		//System.out.println(Arrays.toString(new_arr));
+
+
+		//質問７：
+		//char[] input = new char[] { ' ', ' ', 'A', 'b', ' ', ' ', 'C', ' ' };
+		//char[] output = trim(input);
+		//System.out.println("Question07：");
+		//printArrayChar(output);
+
+
+		//質問８：
+		//int[] a = new int[] {2,1,34,52,36,34,6,15,67,100,456};
+		 		//System.out.println("Question08：");
+		 		//printArrayInt(bubbleSort(a));
+
+		//質問９：
+//		int[] a = {1,2,3,4};
+//		int[] b = {5,6,7,8};
+//		int[] c = new int[a.length + b.length];
+//
+//		System.out.print("arrayAは:");
+//		for (int i = 0; i < a.length; i++) {
+//			System.out.print(a[i] + "");
+//		}
+//		System.out.println();
+//		System.out.print("arrayBは: ");
+//		for (int i = 0; i < b.length; i++) {
+//			System.out.print(b[i] + "");
+//		}
+//		System.out.println();
+//		for (int i = 0; i < a.length; i++) {
+//			c[i] = a[i];
+//		}
+//		int n = 0;
+//		for(int i = a.length; i < c.length; i++) {
+//			c[i] = b[n];
+//			n++;
+//		}
+//		System.out.print("arrayC: (arrayA+arrayB with asc);");
+//		Arrays.sort(c);
+//		for (int i = 0; i < c.length; i++) {
+//			System.out.print(c[i] + "");
+//		}
+
+}
 }
