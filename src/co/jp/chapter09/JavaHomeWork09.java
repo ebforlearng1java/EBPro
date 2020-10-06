83c6ee82b2f0aad3da1f78c9bdb28165daed7982
zyc <zyc@DESKTOP-H0B0IIC>
2020-10-07 01:11:13 +0900

    JavaHomeWork09

--- src/co/jp/chapter09/JavaHomeWork09.java
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -2,4 +2,43 @@ package co.jp.chapter09;
 
 public class JavaHomeWork09 {
 
+	
+	//質問１︓以下ソースにラムダ式の使う場所を解釈してください。
+	//import java.awt.event.ActionEvent;
+	//import java.awt.event.ActionListener;
+	//import javax.swing.JButton;
+	//import javax.swing.JFrame;
+	
+	//public class GUISample {
+	//	public static void main(String[] args) {
+	//	JFrame window = new JFrame("DCNet Java 教育");
+	//	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//	window.setSize(800, 600);
+	//	JButton btn = new JButton("hello world");
+	//	window.getContentPane().add(btn);
+	
+	
+	//	btn.addActionListener((ActionEvent e) -> {⇒ラダム式：addActionListenerってメソッドを実装する、引数はActionEvent型のe
+	//	System.out.println("ボタンクリックしました。");
+	//	});
+	
+	//	window.setVisible(true);
+	//	}
+		
+	//質問２︓以下ソースに匿名クラスの利用する行は？
+	
+	//public class Kicker {
+		//public static void main(String... args) {
+		//new Thread() {  ⇒匿名クラス
+		//@Override
+		//public void run() {
+		//System.out.println("thread running...");
+		//}
+		//}.start();
+		//}
+		//}
+	
+	
+	
+	
 }
