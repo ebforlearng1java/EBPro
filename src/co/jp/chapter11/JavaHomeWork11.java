bce5bd2694570dd1a0ce2d403a37a5aac8cffae2
user <user@192.168.0.103>
2020-10-06 22:46:51 +0900

    2020/10/06

--- src/co/jp/chapter11/JavaHomeWork11.java
+++ src/co/jp/chapter11/JavaHomeWork11.java
@@ -4,6 +4,10 @@ public class JavaHomeWork11 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		//質問１駅クラス（Station）、路線クラス（Line）を作成して。路線をインスタンス化し
+		//てオブジェクト「山手線」を作成してください。「山手線」に各駅情報を追加してください。
 
 	}
 
