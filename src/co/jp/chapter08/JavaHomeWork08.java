b65de984a0842293c14ff1c0a0c5fb07c70516ab
zyc <zyc@DESKTOP-H0B0IIC>
2020-10-06 22:25:45 +0900

    JavaHomeWork08

---
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -0,0 +1,26 @@
+package co.jp.chapter08;
+
+public class JavaHomeWork08 {
+	public static void main(String[] args) {
+	//質問１︓以下コードの出力するを回答してください。
+	String x = "a";
+	String y = "a";
+	System.out.println(x == y); // true 
+	System.out.println(x.equals(y)); // true 
+	String empty = "";
+	String nullString = null;
+	System.out.println(nullString.equals(empty));// false;null不等于空字符串
+	
+	}
+	
+	//質問２︓以下コードの出⼒を回答してください。
+	
+	
+	
+	
+	
+	
+
+}
+
+
