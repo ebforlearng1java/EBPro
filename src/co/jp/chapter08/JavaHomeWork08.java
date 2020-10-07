02bde90f811f41869a74398ed40616f45194d008
Qiuqiu <Qiuqiu@VAIO>
2020-10-07 20:52:24 +0800

    宿題０８

---
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -0,0 +1,38 @@
+package co.jp.chapter08;
+
+public class JavaHomeWork08 {
+
+ 	public static void main(String[] args) {
+ 		// TODO 自動生成されたメソッド・スタブ
+
+        //質問１：
+//        String x = "a";
+// 		String y = "a";
+// 		System.out.println(x == y);  //TRUE 同じ位置の"a"を指定している
+// 		System.out.println(x.equals(y)); // TRUE 文字列内容は一致する
+//
+// 		String empty = "";
+// 		String nullString = null;
+// 		System.out.println(nullString.equals(empty)); 	//FALSE nullの場合に、equalsを使えない
+
+
+        //質問２：
+// 		System.out.println(x == y);  // FALSE メモリアドレスは一致しない
+// 		System.out.println(x.equals(y));  // TRUE equalsはx,yを比較する
+
+
+        //質問３：
+ 	    // Kicker.java
+//	    public class Kicker {
+//	    public static void print(Child v) {   ⇒修正後: public static void print(Parent v) {
+//	    v.hello();
+//		  }
+//		  public static void main(String...args) {
+// 		 		  print(new Parent());
+// 		 		  print(new Child());
+//		  }
+//		 }
+
+}
+
+}
