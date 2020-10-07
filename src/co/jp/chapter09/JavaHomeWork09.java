9364af7dc0c2118a2c32dddbcec231f25b96e1c4
Qiuqiu <Qiuqiu@VAIO>
2020-10-07 21:11:53 +0800

    宿題０９

---
+++ src/co/jp/chapter09/JavaHomeWork09.java
@@ -0,0 +1,21 @@
+package co.jp.chapter09;
+
+public class JavaHomeWork09 {
+ 	public static void main(String[] args) {
+ 		// TODO 自動生成されたメソッド・スタブ
+
+
+ 		//質問１：
+// 		btn.addActionListener((ActionEvent e) -> {
+// 			System.out.println("ボタンクリックしました。");
+// 			});
+//答：上記のところにラムダ式を使う
+// 	   btnを押すと、addActionListenerを呼び出し、ActionEvent型の引数eを出すと、"ボタンクリックしました"を出力
+
+
+ 		//質問２：
+ //答：匿名クラスは new Thread() {
+
+ 	}
+ 	}
+
