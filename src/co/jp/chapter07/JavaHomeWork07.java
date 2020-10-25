1c36b0c9d643387bbbb5115d1eb300e60a2a53fa
LIN <LIN@iceland>
2020-10-26 01:16:59 +0900

    20201007 巩 chapter07 質問回答

--- src/co/jp/chapter07/JavaHomeWork07.java
+++ src/co/jp/chapter07/JavaHomeWork07.java
@@ -1,10 +1,81 @@
+/**ーーーーーーーーーーーーーーーーー07 OOP - その１ーーーーーーーーーーーーーーーーー */
 package co.jp.chapter07;
 
 public class JavaHomeWork07 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	//	質問１：以下コードのコンパイルエラー理由を教えてください。
+	//	// Pet.java
+	//	public class Pet {
+	//	 public String name;
+	//
+	//	 public void setName(String value) {
+	//	 this.name = value;
+	//	 }
+	//	}
+	//	// Kicker.java
+	//	public class Kicker {
+	//	 public static void main(String...args) {
+	//	 Pet cat = new Pet();
+	//	 cat.setAge(1234);
+	//	 }
+	//	}
+	//回答：Pet.javaにsetAgeが定義されていない。
 
+	//	質問２：Mathクラスを作成してください。以下メソッドを定義します。
+	//	1. 配列のMAXを求める静的メソッドを定義。（引数は int 配列）
+	//	2. 配列のMINを求める静的メソッドを定義。（引数は int 配列）
+	//	3. 配列の平均値を求める静的メソッドを定義。（引数は int 配列）
+	//	4. mainメソッドで上記それぞれメソッドを呼び出してください。
+	//	以下はメソッドの定義のソースの抜枠です。
+	//	public class Math {
+	//		 public static int max(int[] input) {
+	//		 //...
+	//		 }
+	//	}
+	//回答：Match.javaに1－3のメソッドを作成して、このクラスのmainメソッドで呼び出している。
+
+	//	2.行列計算。2D行列クラスのコンストラクタには１配列引数設定してください。
+	//	public class Matrix {
+	//		 private final int[][] data;
+	//		 /** construct method */
+	//		 public Matrix(int[][] data) {
+	//		 this.data = data;
+	//		 }
+	//		 // 行列 ADD
+	//		 public Matrix add(Matrix b) {
+	//		 //....
+	//		 return ???
+	//		 }
+	//		}
+	//  答え：Matrix.javaにaddメソッドを作成して、このクラスのmainメソッドで呼び出している。
+
+	public static void main(String[] args) {
+		int[] array1 = new int[] { 2, 3, 4, 5, 6, 2, 1 };
+
+		System.out.println("配列のMAX値：" + Math.max(array1));
+		System.out.println("配列のMIN値：" + Math.min(array1));
+		System.out.println("配列の平均値：" + Math.ave(array1));
+
+		int[][] array21 = new int[][] {
+				{ 1, 2 },
+				{ 3, 4 }
+		};
+		int[][] array22 = new int[][] {
+				{ 5, 6 },
+				{ 7, 8 }
+		};
+
+		Matrix a = new Matrix(array21);
+		Matrix b = new Matrix(array22);
+
+		Matrix c = a.add(b);
+		System.out.println("行列計算:");
+		for (int[] array : c.getData()) {
+			for (int item : array) {
+				System.out.print(item + ",");
+			}
+			System.out.println();
+		}
 	}
 
 }
---
+++ src/co/jp/chapter07/Math.java
@@ -0,0 +1,47 @@
+package co.jp.chapter07;
+
+public class Math {
+	//配列のMax値を求めるメソッド
+	public static int max(int[] datas) {
+		if (datas == null || datas.length == 0) {
+			return 0;
+		}
+		int max = datas[0];
+		for (int item : datas) {
+			if (max < item) {
+				max = item;
+			}
+		}
+		return max;
+	}
+
+	//配列のMin値を求めるメソッド
+	public static int min(int[] datas) {
+		if (datas == null || datas.length == 0) {
+			return 0;
+		}
+		int min = datas[0];
+		for (int item : datas) {
+			if (min > item) {
+				min = item;
+			}
+		}
+		return min;
+	}
+
+	//配列の平均値を求めるメソッド
+	public static int ave(int[] datas) {
+		if (datas == null || datas.length == 0) {
+			return 0;
+		}
+
+		int sum = 0;
+		for (int item : datas) {
+			sum = sum + item;
+		}
+		int ave = sum / datas.length;
+
+		return ave;
+	}
+
+}
