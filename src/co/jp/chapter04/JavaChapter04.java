98e5368b2b18959d3a525dc3986883ae62752e6e
Qiuqiu <Qiuqiu@VAIO>
2020-10-25 22:40:30 +0800

    期末

--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -5,6 +5,38 @@ public class JavaChapter04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		
+		int[][] array7 = new int[][] { 
+			 			{1,2}, 
+			 			{3,4}, 
+					}; 
+			 		int[][] array8 = new int[][] { 
+						{5,6}, 
+		 			{7,8} 
+					}; 
+			 		int [][] arrC = new int[2][2]; 
+			
+			 
+					for (int i = 0 ;i <2; i++) { 
+			 			for (int j =0; j<2; j++) { 
+			 				arrC[i][j] = array7[i][j] + array8[i][j]; 
+							System.out.print(arrC[i][j] + ","); 
+			 			} 
+			 			System.out.println(); 
+			 		} 
+
+					int[][] array = {
+							{1,2,3,4},
+							{10,20,30,40,50,60},
+							{1000,10000}
+					};
+				int sum = 0;
+					for (int i=0; i<array.length; i++) {
+						for(int s=0; s<array[i].length; s++) {
+							sum += array[i][s];
+					}
+					}
+
 	}
 
 }
--- src/co/jp/chapter06/JavaChapter06.java
+++ src/co/jp/chapter06/JavaChapter06.java
@@ -5,6 +5,35 @@ public class JavaChapter06 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//呼び出し
+		JavaChapter06 obj = new JavaChapter06();
+		obj.say();
+			System.out.println("main の Say hello");
+		}
+		
+		
+	//名前
+	String namae = "ZWL";
+	//年齢
+	int nennrei = 25;
+	//会社アドレス
+	String kaishaAdress = "友の門";
+	
+	//
+	void say() {
+		System.out.println("名前は" + namae);
+		System.out.println("年齢は" + nennrei);
+		System.out.println("会社アドレスは" + kaishaAdress);
 	}
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
