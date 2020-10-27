ae0592a2f1ba12f9b26045782a242e3a0462e903
Everbe <a410526712@yahoo.co.jp>
2020-10-27 19:54:31 +0900

    10/27

--- src/co/jp/chapter11/JavaHomeWork11.java
+++ src/co/jp/chapter11/JavaHomeWork11.java
@@ -1,7 +1,8 @@
 package co.jp.chapter11;
 
 public class JavaHomeWork11 {
-
+	int customer = 31;
+	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
@@ -9,7 +10,18 @@ public class JavaHomeWork11 {
 //		質問１︓駅クラス（Station）、路線クラス（Line）を作成して。路線をインスタンス化し
 //		てオブジェクト「⼭⼿線」を作成してください。「⼭⼿線」に各駅情報を追加してくださ
 //		い。
+		System.out.println("Line.java Station.javaを参照してください");
 		
+//		質問２︓質問１に作成した⼭⼿線の駅をforeach ⽂を繰り返して、新橋駅は路線内である
+//		かを判断する。
+		System.out.println("Line.javaを参照してください");
+
+//		質問３︓10両編成している通勤電⾞、各⾞の定員は5⼈です。31⼈の乗⾞の状況をプログ
+//		ラミングしてください。（for⽂で旅客作成しても構わない）
+		
+		for(int i=0;i<10;i++) {
+			
+		}
 		
 
 	}
--- src/co/jp/chapter11/Line.java
+++ src/co/jp/chapter11/Line.java
@@ -1,5 +1,20 @@
 package co.jp.chapter11;
 
-public class Line extends Station{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		Station station = new Station();
+
+		for (int i = 0; i < station.station.length; i++) {
+			list.add(station.station[i]);
+			if(list.get(i) == "新橋") {
+				System.out.println("新橋駅は山手線の中にある");
+			}
+		}
+	}
 
 }
