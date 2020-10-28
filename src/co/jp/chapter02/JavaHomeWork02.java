aca79817be5d8cdb55b44613494faf5a84377b87
admin <admin@LAPTOP-PB4SIJJC>
2020-10-28 23:17:30 +0900

    1028

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -10,10 +10,7 @@ public class JavaHomeWork02 {
 		//質問４:1.0 + 2.0 = 3.0
 		//       1.0 + "2.0" = 1.02.0
 		//質問５:a
-		public static void main(String[] args) {
-            int a = 10;
-            System.out.println("a");
-            }
+
 
 
 
